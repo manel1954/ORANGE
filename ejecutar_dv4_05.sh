4235b2e22720d8cdd4b457b6e5bd9eb18863af06
Manel <ea3eiz@ea3eiz.com>
2023-03-09 10:21:33 +0100

    Update ejecutar_dv4_05.sh

--- ejecutar_dv4_05.sh
+++ ejecutar_dv4_05.sh
@@ -7,7 +7,9 @@ sed -i "11c Name[es_ES]=Cerrar DV4mini" /home/orangepi/Desktop/DV4mini.desktop
 sed -i "4c DV4mini=ON" /home/orangepi/status.ini
 
 cd /home/orangepi/dv4mini
-mono dv4mini.exe
+
+su root -c ‘mono dv4mini.exe’
+
 
 sed -i "6c Exec=sh ejecutar_dv4_05.sh" /home/orangepi/Desktop/DV4mini.desktop
 sed -i "7c Icon=/home/orangepi/dv4mini/dv4k.png" /home/orangepi/Desktop/DV4mini.desktop
