28696810bda2bf1e26c42e8f82ca6dd0df6ab1e7
Manel <ea3eiz@ea3eiz.com>
2023-03-10 09:39:59 +0100

    Update colocar_iconos.sh

--- colocar_iconos.sh
+++ colocar_iconos.sh
@@ -14,8 +14,11 @@
 
 # recupera los iconos que se han borrado y se han convertido en icono de un libro
 # pero no quita los semaforos que se queden encendidos por error al reiniciar
-sudo cp /home/orangepi/ORANGE/Desktop/*.* /home/orangepi/Desktop
-sudo chmod 777 -R /home/orangepi/Desktop
+
+sudo rm /home/orangepi/Desktop/*.*
+
+#sudo cp /home/orangepi/ORANGE/Desktop/*.* /home/orangepi/Desktop
+#sudo chmod 777 -R /home/orangepi/Desktop
 
 cp /home/orangepi/icons.screen0-1904x1021.rc /home/orangepi/.config/xfce4/desktop
 xfdesktop -R                           
