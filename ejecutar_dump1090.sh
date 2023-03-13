edfdcf288c2847206e4660549fba38145296895c
Manel <ea3eiz@ea3eiz.com>
2023-03-13 16:57:47 +0100

    1

--- ejecutar_dump1090.sh
+++ ejecutar_dump1090.sh
@@ -21,4 +21,4 @@ fi
 
 sed -i "6c Exec=sh ejecutar_dump1090.sh" /home/orangepi/Desktop/BlueDV.desktop
 sed -i "7c Icon=/home/orangepi/ORANGE/ICONO_AVION_OFF.png" /home/orangepi/Desktop/BlueDV.desktop
-sed -i "10c Name[es_ES]=Ejecutar Dump1090" /home/orangepi/Desktop/BlueDV.desktop
+sed -i "10c Name[es_ES]=Abrir Dump1090" /home/orangepi/Desktop/BlueDV.desktop
