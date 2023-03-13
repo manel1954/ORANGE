7974119a3ff070a8f4d710ea50bb6836c11c84f4
Manel <ea3eiz@ea3eiz.com>
2023-03-13 18:05:55 +0100

    Update menu_autoarranque_05.sh

--- menu_autoarranque_05.sh
+++ menu_autoarranque_05.sh
@@ -254,7 +254,7 @@ dstar=`grep "D-STAR" /home/orangepi/autoarranque.ini`
 
 
 
-echo -n "\33[1;36m   202)\33[1;33m  Poner dump1090 en el autoarranque     - "
+echo -n "\33[1;36m   202)\33[1;33m  Poner DUMP1090 en el autoarranque     - "
 dump1090=`grep "DUMP1090" /home/orangepi/autoarranque.ini`
 dump1090=`expr substr $dump1090 10 3`
 if [ $dump1090 = "OFF" ]
