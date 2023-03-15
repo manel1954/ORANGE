6e14bea1a9bcdda4aafa2477ceb189f6d6709d46
Manel <ea3eiz@ea3eiz.com>
2023-03-15 16:12:43 +0100

    1

--- cerrar_plus_05.sh
+++ cerrar_plus_05.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo killall MMDVMPLUS
-sed -i "6c Exec=mate-terminal -x sh ejecutar_plus_05.sh" /home/orangepi/Desktop/DMRPLUS.desktop
+sed -i "6c Exec=mate-terminal --geometry 105x14+22+900 -x sh ejecutar_plus_05.sh" /home/orangepi/Desktop/DMRPLUS.desktop
 sed -i "7c Icon=/home/orangepi/ORANGE/ICONO_DMRPLUS.png" /home/orangepi/Desktop/DMRPLUS.desktop
 sed -i "10c Name[es_ES]=Abrir DMR+" /home/orangepi/Desktop/DMRPLUS.desktop
 
