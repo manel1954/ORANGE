3f2a8cedf90da4bf06304d1e3f7a9fc388008825
Manel <ea3eiz@ea3eiz.com>
2023-03-16 00:59:36 +0100

    1

--- cerrar_bm_05.sh
+++ cerrar_bm_05.sh
@@ -4,6 +4,6 @@ sudo killall MMDVMBM
 cd /home/orangepi/ORANGE
 sed -i "7c MMDVMBM=OFF" /home/orangepi/status.ini
 
-sed -i "6c Exec=sh ejecutar_bm_05.sh" /home/orangepi/Desktop/BM.desktop
+sed -i "6c Exec=mate-terminal --geometry 104x16+1149+880 --title=BRANDMEISTER -x sh ejecutar_bm_05.sh" /home/orangepi/Desktop/BM.desktop
 sed -i "7c Icon=/home/orangepi/ORANGE/DMR.png" /home/orangepi/Desktop/BM.desktop
 sed -i "10c Name[es_ES]=Abrir BM" /home/orangepi/Desktop/BM.desktop
