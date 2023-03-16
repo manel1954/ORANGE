0867cd5529d0257d7898e05301c42787a3d502d5
Manel <ea3eiz@ea3eiz.com>
2023-03-16 11:53:57 +0100

    Update ejecutar_svxlink_05.sh

--- ejecutar_svxlink_05.sh
+++ ejecutar_svxlink_05.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed -i "6c Exec=mate-terminal -x sh cerrar_svxlink_05.sh" /home/orangepi/Desktop/Abrir_SVXLink.desktop
+sed -i "6c Exec=sh cerrar_svxlink_05.sh" /home/orangepi/Desktop/Abrir_SVXLink.desktop
 sed -i "7c Icon=/home/orangepi/ORANGE/SVXLINK_ON.png" /home/orangepi/Desktop/Abrir_SVXLink.desktop
 sed -i "4c Name[es_ES]=Cerrar SVXLINK" /home/orangepi/Desktop/Abrir_SVXLink.desktop
 
