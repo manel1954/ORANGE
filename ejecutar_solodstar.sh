16022ee438f4ae73ee07c78f5a6477fb53740a7c
Manel <ea3eiz@ea3eiz.com>
2023-03-16 13:01:23 +0100

    Update ejecutar_solodstar.sh

--- ejecutar_solodstar.sh
+++ ejecutar_solodstar.sh
@@ -15,13 +15,10 @@ sed -i "13c SOLODSTAR=ON" /home/orangepi/status.ini
 cd /home/orangepi/MMDVMHost
 echo "{$VERDE}"
 
-mate-terminal --geometry 104x16+1151+880 --title=SOLODSTAR -x ./MMDVMDSTAR MMDVMDSTAR.ini & 
+./MMDVMDSTAR MMDVMDSTAR.ini & 
 ircddbgateway -gui
 
 sed -i "6c Exec=mate-terminal -x sh -c 'cd /home/orangepi/ORANGE/;sh ejecutar_solodstar.sh'" /home/orangepi/Desktop/AbrirsoloDstar.desktop
 sed -i "7c Icon=/home/orangepi/ORANGE/SOLO_D-STAR.png" /home/orangepi/Desktop/AbrirsoloDstar.desktop
 sed -i "11c Name[es_ES]=Abrir solo D-STAR" /home/orangepi/Desktop/AbrirsoloDstar.desktop
 sed -i "13c SOLODSTAR=OFF" /home/orangepi/status.ini
-
-
-sed -i "6c Exec=mate-terminal --geometry 104x16+1151+880 --title=RADIO -x sh ejecutar_mmdvm_05.sh" /home/orangepi/Desktop/MMDVM.desktop
