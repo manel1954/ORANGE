f28080f56a69798a9ebb9392be2155c7ed03fd20
Manel <ea3eiz@ea3eiz.com>
2023-03-16 01:30:52 +0100

    Update ejecutar_DMR2YSF.sh

--- ejecutar_DMR2YSF.sh
+++ ejecutar_DMR2YSF.sh
@@ -5,10 +5,10 @@ sed -i "7c Icon=/home/orangepi/ORANGE/DMR2YSF_ON.png" /home/orangepi/Desktop/Abr
 sed -i "10c Name[es_ES]=Cerrar DMR2YSF" /home/orangepi/Desktop/Abrir_DMR2YSF.desktop
 
 cd /home/orangepi/DMR2YSF
-mate-terminal --geometry 101x8+1271+665 --title=DMR2YSF -x ./DMR2YSF DMR2YSF.ini &
+mate-terminal --geometry 101x8+1273+665 --title=DMR2YSF -x ./DMR2YSF DMR2YSF.ini &
 
 cd /home/orangepi/MMDVMHost
-mate-terminal --geometry 101x8+1272+796 --title=MMDVMDMR2YSF -x ./DMR2YSF MMDVMDMR2YSF.ini &
+mate-terminal --geometry 101x8+1273+797 --title=MMDVMDMR2YSF -x ./DMR2YSF MMDVMDMR2YSF.ini &
 
 cd /home/orangepi/YSFClients/YSFGateway
 mate-terminal --geometry 101x9+1273+987 --title=YSFGATEWAY -x ./YSFGateway YSFGateway.ini
