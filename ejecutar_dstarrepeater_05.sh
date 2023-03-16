596185adc54e789c58b995e1a4d7d02986adaf81
Manel <ea3eiz@ea3eiz.com>
2023-03-16 18:18:57 +0100

    1

--- ejecutar_dstarrepeater_05.sh
+++ ejecutar_dstarrepeater_05.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-
 cd /home/orangepi/ORANGE
 
+sed -i "6c Exec=sh cerrar_dstarrepeater_05.sh" /home/orangepi/Desktop/Dstar_solo.desktop
+sed -i "7c Icon=/home/orangepi/ORANGE/REPEATER_ON.png" /home/orangepi/Desktop/Dstar_solo.desktop
+sed -i "10c Name[es_ES]=Cerrar D-STAR Repeater" /home/orangepi/Desktop/Dstar_solo.desktop
 
 
-sed -i "4c Exec=sh -c 'cd /home/orangepi/ORANGE; sudo sh cerrar_dstarrepeater_30.sh'" /home/orangepi/Desktop/Abrir_D-STARRepeater
-sed -i "5c Icon=/home/orangepi/ORANGE/REPEATER_ON.png" /home/orangepi/Desktop/Abrir_D-STARRepeater
-sed -i "10c Name[es_ES]=Cerrar D-STAR Repeater" /home/orangepi/Desktop/Abrir_D-STARRepeater
 sed -i "9c dstarrepeater=ON" /home/orangepi/status.ini	
+
 sudo dstarrepeater
 
 
