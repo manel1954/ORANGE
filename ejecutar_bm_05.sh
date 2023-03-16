607251434b10efb3ae5a384a8c526644fedd4855
Manel <ea3eiz@ea3eiz.com>
2023-03-16 01:02:04 +0100

    1

--- ejecutar_bm_05.sh
+++ ejecutar_bm_05.sh
@@ -18,7 +18,7 @@ sudo killall MMDVMBM
 cd /home/orangepi/ORANGE
 sed -i "7c MMDVMBM=OFF" /home/orangepi/status.ini
 
-sed -i "6c Exec=mate-terminal --geometry 104x16+1149+880 -x sh ejecutar_bm_05.sh" /home/orangepi/Desktop/BM.desktop
+sed -i "6c Exec=mate-terminal --geometry 104x16+1151+880 -x sh ejecutar_bm_05.sh" /home/orangepi/Desktop/BM.desktop
 sed -i "7c Icon=/home/orangepi/ORANGE/DMR.png" /home/orangepi/Desktop/BM.desktop
 sed -i "10c Name[es_ES]=Abrir BM" /home/orangepi/Desktop/BM.desktop
 
