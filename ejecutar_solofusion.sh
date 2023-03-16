6d89f2656fa04a7575df2fdecb8d987650b9bf0c
Manel <ea3eiz@ea3eiz.com>
2023-03-16 15:01:23 +0100

    Update ejecutar_solofusion.sh

--- ejecutar_solofusion.sh
+++ ejecutar_solofusion.sh
@@ -13,11 +13,11 @@ sed -i "7c Icon=/home/orangepi/ORANGE/SOLO_YSF_ON.png" /home/orangepi/Desktop/Ab
 sed -i "11c Name[es_ES]=Cerrar solo FUSION" /home/orangepi/Desktop/AbrirsoloYSF.desktop
 
 sed -i "12c SOLOFUSION=ON" /home/orangepi/status.ini
-echo "\33[38;5;138m"
+
 cd /home/orangepi/YSFClients/YSFGateway
-echo "\33[38;5;138m"
+
 mate-terminal --geometry 101x8+1277+797 --title=YSFGateway -x ./YSFGateway YSFGateway.ini & 
-echo "\33[38;5;138m"
+
 cd /home/orangepi/MMDVMHost
 
 mate-terminal --geometry 101x9+1277+987 --title=MMDVMFUSION -x sudo ./MMDVMFUSION MMDVMFUSION.ini
