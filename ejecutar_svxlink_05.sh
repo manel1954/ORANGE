dfa6cb72b6a1e5321844cf284aa2b959d8670c0d
Manel <ea3eiz@ea3eiz.com>
2023-03-16 12:11:51 +0100

    1

--- ejecutar_svxlink_05.sh
+++ ejecutar_svxlink_05.sh
@@ -6,15 +6,4 @@ sed -i "4c Name[es_ES]=Cerrar SVXLINK" /home/orangepi/Desktop/Abrir_SVXLink.desk
 
 sed -i "8c SVXLINK=ON" /home/orangepi/status.ini
 
-    #[Desktop Entry]
-    #Version=1.0
-    #Type=Application
-    #Name=YSFGateway
-    #Comment=
-    #Exec=mate-terminal -x sh ejecutar_ysf_05.sh
-    #Icon=/home/orangepi/ORANGE/C4FM.png
-    #Path=/home/orangepi/ORANGE
-    #Terminal=false
-    #StartupNotify=false
-    #Name[es_ES]=Abrir YSF
 sudo svxlink
