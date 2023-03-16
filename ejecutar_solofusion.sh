f44b5166c7ee70d63b8353789b7ecc8f31f5de05
Manel <ea3eiz@ea3eiz.com>
2023-03-16 15:09:49 +0100

    1

--- ejecutar_solofusion.sh
+++ ejecutar_solofusion.sh
@@ -7,7 +7,7 @@ AMARILLO="\033[1;33m"
 CIAN="\033[1;36m"
 GRIS="\033[0m"
 MARRON="\33[38;5;138m"
-echo "{$MARRON}"
+
 sed -i "6c Exec=sudo sh cerrar_solofusion.sh" /home/orangepi/Desktop/AbrirsoloYSF.desktop
 sed -i "7c Icon=/home/orangepi/ORANGE/SOLO_YSF_ON.png" /home/orangepi/Desktop/AbrirsoloYSF.desktop
 sed -i "11c Name[es_ES]=Cerrar solo FUSION" /home/orangepi/Desktop/AbrirsoloYSF.desktop
@@ -19,6 +19,6 @@ cd /home/orangepi/YSFClients/YSFGateway
 mate-terminal --geometry 101x8+1277+797 --title=YSFGateway -x ./YSFGateway YSFGateway.ini & 
 
 cd /home/orangepi/MMDVMHost
-
+echo "{$VERDE}"
 mate-terminal --geometry 101x9+1277+987 --title=MMDVMFUSION -x sudo ./MMDVMFUSION MMDVMFUSION.ini
 
