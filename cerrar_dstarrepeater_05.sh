45ab277a98ff18c173703fcbbbe54f4397dce5bb
Manel <ea3eiz@ea3eiz.com>
2023-03-16 18:23:37 +0100

    Update cerrar_dstarrepeater_05.sh

--- cerrar_dstarrepeater_05.sh
+++ cerrar_dstarrepeater_05.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sed -i "6c Exec=sh ejecutar_dstarrepeater_05.sh" /home/orangepi/Desktop/Dstar_solo.desktop
-sed -i "7c Icon=/home/orangepi/ORANGE/DMR.png" /home/orangepi/Desktop/Dstar_solo.desktop
+sed -i "7c Icon=/home/orangepi/ORANGE/repeater.png" /home/orangepi/Desktop/Dstar_solo.desktop
 sed -i "10c Name[es_ES]=Abrir D-STAR Repeater" /home/orangepi/Desktop/Dstar_solo.desktop
 
 #cierra ircDDBGateway
