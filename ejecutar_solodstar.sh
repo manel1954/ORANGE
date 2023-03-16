49e27aac9604fd9dc3335b8ef86dc9f9d972905e
Manel <ea3eiz@ea3eiz.com>
2023-03-16 13:12:25 +0100

    Update ejecutar_solodstar.sh

--- ejecutar_solodstar.sh
+++ ejecutar_solodstar.sh
@@ -18,7 +18,7 @@ echo "{$VERDE}"
 sudo ./MMDVMDSTAR MMDVMDSTAR.ini & 
 ircddbgateway -gui
 
-sed -i "6c Exec=mate-terminal -x sh -c 'cd /home/orangepi/ORANGE/;sh ejecutar_solodstar.sh'" /home/orangepi/Desktop/AbrirsoloDstar.desktop
+sed -i "6c Exec=mate-terminal --geometry 104x16+1151+880 --title=SOLODSTAR -x sh ejecutar_solodstar.sh" /home/orangepi/Desktop/AbrirsoloDstar.desktop
 sed -i "7c Icon=/home/orangepi/ORANGE/SOLO_D-STAR.png" /home/orangepi/Desktop/AbrirsoloDstar.desktop
 sed -i "11c Name[es_ES]=Abrir solo D-STAR" /home/orangepi/Desktop/AbrirsoloDstar.desktop
 sed -i "13c SOLODSTAR=OFF" /home/orangepi/status.ini
