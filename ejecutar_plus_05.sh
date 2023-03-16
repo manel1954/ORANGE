705dcb747eeb7d346750cafa700d5e4729da41d9
Manel <ea3eiz@ea3eiz.com>
2023-03-16 01:07:19 +0100

    1

--- ejecutar_plus_05.sh
+++ ejecutar_plus_05.sh
@@ -16,7 +16,7 @@ echo "***********************************************"
 echo "\33[1;37m"
 sudo ./MMDVMPLUS MMDVMPLUS.ini
  
-sed -i "6c Exec=mate-terminal --geometry 105x16+22+880 --title=DMRPLUS -x sh ejecutar_plus_05.sh" /home/orangepi/Desktop/DMRPLUS.desktop
+sed -i "6c Exec=mate-terminal --geometry 105x16+22+880 --title=DMR+ -x sh ejecutar_plus_05.sh" /home/orangepi/Desktop/DMRPLUS.desktop
 sed -i "7c Icon=/home/orangepi/ORANGE/ICONO_DMRPLUS.png" /home/orangepi/Desktop/DMRPLUS.desktop
 sed -i "10c Name[es_ES]=Abrir DMR+" /home/orangepi/Desktop/DMRPLUS.desktop
 
