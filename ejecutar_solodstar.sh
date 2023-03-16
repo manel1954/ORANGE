c685515165f742d4d51f23af04e6700739a75e79
Manel <ea3eiz@ea3eiz.com>
2023-03-16 13:03:11 +0100

    Update ejecutar_solodstar.sh

--- ejecutar_solodstar.sh
+++ ejecutar_solodstar.sh
@@ -22,3 +22,4 @@ sed -i "6c Exec=mate-terminal -x sh -c 'cd /home/orangepi/ORANGE/;sh ejecutar_so
 sed -i "7c Icon=/home/orangepi/ORANGE/SOLO_D-STAR.png" /home/orangepi/Desktop/AbrirsoloDstar.desktop
 sed -i "11c Name[es_ES]=Abrir solo D-STAR" /home/orangepi/Desktop/AbrirsoloDstar.desktop
 sed -i "13c SOLODSTAR=OFF" /home/orangepi/status.ini
+
