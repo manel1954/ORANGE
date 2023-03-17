423f3292f25b153980511fcd91d9916417c3bbcf
Manel <ea3eiz@ea3eiz.com>
2023-03-17 11:56:11 +0100

    Update cerrar_ambe_server.sh

--- cerrar_ambe_server.sh
+++ cerrar_ambe_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i "6cExec=sh ejecutar_ambe_server.sh" /home/orangepi/Desktop/Abrir_ambe_server.desktop
+sed -i "6c Exec=sh ejecutar_ambe_server.sh" /home/orangepi/Desktop/Abrir_ambe_server.desktop
 sed -i "7c Icon=/home/orangepi/ORANGE/AMBE_SERVER.png" /home/orangepi/Desktop/Abrir_ambe_server.desktop
 sed -i "10c Name[es_ES]=Abrir AMBE SERVER" /home/orangepi/Desktop/Abrir_ambe_server.desktop
 
