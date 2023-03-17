c7cbccc59578a97c58de645fc1e59cdcd0265219
Manel <ea3eiz@ea3eiz.com>
2023-03-17 12:00:46 +0100

    Update ejecutar_ambe_server.sh

--- ejecutar_ambe_server.sh
+++ ejecutar_ambe_server.sh
@@ -11,7 +11,7 @@ puerto_modem=$(awk "NR==2" /home/orangepi/ambe_server.ini)
 baut_rate=$(awk "NR==3" /home/orangepi/ambe_server.ini)
 cd /home/orangepi/AMBEServer
 sudo killall AMBEserver
-mate-terminal --geometry 74x18+696+50 -x sudo. /AMBEserver -p $puerto_router -i $puerto_modem -s $baut_rate
+mate-terminal --geometry 74x18+696+50 -x sudo ./AMBEserver -p $puerto_router -i $puerto_modem -s $baut_rate
 
 sed -i "6c Exec=sh ejecutar_ambe_server.sh" /home/orangepi/Desktop/Abrir_ambe_server.desktop
 sed -i "7c Icon=/home/orangepi/ORANGE/AMBE_SERVER.png" /home/orangepi/Desktop/Abrir_ambe_server.desktop
